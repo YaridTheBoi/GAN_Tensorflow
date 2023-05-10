9a5f070cca219b83516a1bb0c1020a60e060babe
YaridTheBoi <kacper.chrostowski@gmail.com>
2023-05-10 17:40:08 +0200

    created utils, reduced code, added reqs

--- trainer.py
+++ trainer.py
@@ -5,9 +5,10 @@ import os
 import time
 from keras import layers
 
-import PIL
-import imageio
-from IPython import display
+from utils import make_generator_model, make_discriminator_model
+
+checkpoint_dir = './training_checkpoints'
+os.mkdir(checkpoint_dir)
 
 (train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
 
@@ -17,48 +18,12 @@ train_images = (train_images - 127.5) / 127.5  # Normalize the images to [-1, 1]
 BUFFER_SIZE = 60000
 BATCH_SIZE = 256
 
-# Batch and shuffle the data
+EPOCHS = 50
+noise_dim = 100
+num_examples_to_generate = 16
+
 train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
 
-def make_generator_model():
-    model = tf.keras.Sequential()
-    model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-
-    model.add(layers.Reshape((7, 7, 256)))
-    assert model.output_shape == (None, 7, 7, 256)  # Note: None is the batch size
-
-    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
-    assert model.output_shape == (None, 7, 7, 128)
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-
-    model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-    assert model.output_shape == (None, 14, 14, 64)
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-
-    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
-    assert model.output_shape == (None, 28, 28, 1)
-
-    return model
-
-def make_discriminator_model():
-    model = tf.keras.Sequential()
-    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
-                                        input_shape=[28, 28, 1]))
-    model.add(layers.LeakyReLU())
-    model.add(layers.Dropout(0.3))
-
-    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
-    model.add(layers.LeakyReLU())
-    model.add(layers.Dropout(0.3))
-
-    model.add(layers.Flatten())
-    model.add(layers.Dense(1))
-
-    return model
 
 def discriminator_loss(real_output, fake_output):
     real_loss = cross_entropy(tf.ones_like(real_output), real_output)
@@ -99,11 +64,6 @@ def train(dataset, epochs):
         for image_batch in dataset:
             train_step(image_batch)
 
-        # Produce images for the GIF as you go
-        display.clear_output(wait=True)
-        generate_and_save_images(generator,
-                                epoch + 1,
-                                seed)
 
         # Save the model every 15 epochs
         if (epoch + 1) % 15 == 0:
@@ -111,78 +71,25 @@ def train(dataset, epochs):
 
         print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
 
-    # Generate after the final epoch
-    display.clear_output(wait=True)
-    generate_and_save_images(generator,
-                            epochs,
-                            seed)
-
-
-def generate_and_save_images(model, epoch, test_input):
-    # Notice `training` is set to False.
-    # This is so all layers run in inference mode (batchnorm).
-    predictions = model(test_input, training=False)
-
-    fig = plt.figure(figsize=(4, 4))
-
-    for i in range(predictions.shape[0]):
-        plt.subplot(4, 4, i+1)
-        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
-        plt.axis('off')
-
-    plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
-
-
-# Display a single image using the epoch number
-def display_image(epoch_no):
-    return PIL.Image.open('image_at_epoch_{:04d}.png'.format(epoch_no))
 
 print("START")
 
-
 generator = make_generator_model()
-
-noise = tf.random.normal([1, 100])
-generated_image = generator(noise, training=False)
-
-
-#plt.imshow(generated_image[0, :, :, 0], cmap='gray')
-#plt.show()
-
 discriminator = make_discriminator_model()
-decision = discriminator(generated_image)
-print (decision)
 
 cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-
 
 generator_optimizer = tf.keras.optimizers.Adam(1e-4)
 discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
 
-checkpoint_dir = './training_checkpoints'
+
 checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
 checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
                                 discriminator_optimizer=discriminator_optimizer,
                                 generator=generator,
                                 discriminator=discriminator)
 
-EPOCHS = 50
-noise_dim = 100
-num_examples_to_generate = 16
-
-# You will reuse this seed overtime (so it's easier)
-# to visualize progress in the animated GIF)
 seed = tf.random.normal([num_examples_to_generate, noise_dim])
 
-checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
-#train(train_dataset, EPOCHS)
-generated_image = generator(noise, training=False)
 
-
-plt.imshow(generated_image[0, :, :, 0], cmap='gray')
-plt.show()
-
-
-
-#generate_and_save_images(generator, 50, seed)
-# display_image(EPOCHS)
+train(train_dataset, EPOCHS)
