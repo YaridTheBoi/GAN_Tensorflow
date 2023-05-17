9fd4be70a08e7a7344cc8b9b4d296b1a2f952dea
YaridTheBoi <kacper.chrostowski@gmail.com>
2023-05-17 21:56:46 +0200

    more testing and in-console parameters

--- generator.py
+++ generator.py
@@ -4,28 +4,30 @@ from keras import layers
 import os
 from utils import make_discriminator_model, make_generator_model
 
-IMAGE_SIZE = 120
-noise_dim = 200
 
-discriminator = make_discriminator_model(IMAGE_SIZE)
-generator = make_generator_model(IMAGE_SIZE, noise_dim)
+if __name__ == '__main__':
+    IMAGE_SIZE = int(input("What size were training photos?: "))
+    noise_dim = int(input("What size were noise?: "))
 
-generator_optimizer = tf.keras.optimizers.Adam(1e-4)
-discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+    discriminator = make_discriminator_model(IMAGE_SIZE)
+    generator = make_generator_model(IMAGE_SIZE, noise_dim)
 
-checkpoint_dir = './training_checkpoints'
-checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
-checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
-                                discriminator_optimizer=discriminator_optimizer,
-                                generator=generator,
-                                discriminator=discriminator)
+    generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+    discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
 
-
-checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
-
-noise = tf.random.normal([1, noise_dim])
-generated_image = generator(noise, training=False)
+    checkpoint_dir = './training_checkpoints'
+    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+    checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                    discriminator_optimizer=discriminator_optimizer,
+                                    generator=generator,
+                                    discriminator=discriminator)
 
 
-plt.imshow(generated_image[0, :, :, 0], cmap='gray')
-plt.show()
+    checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
+
+    noise = tf.random.normal([1, noise_dim])
+    generated_image = generator(noise, training=False)
+
+
+    plt.imshow(generated_image[0, :, :, 0], cmap='gray')
+    plt.show()
--- trainer.py
+++ trainer.py
@@ -8,50 +8,6 @@ import cv2
 import numpy as np
 from utils import make_generator_model, make_discriminator_model
 
-checkpoint_dir = './training_checkpoints'
-if not os.path.exists(checkpoint_dir):
-    os.mkdir(checkpoint_dir)
-
-data_dir = './data'
-if not os.path.exists(data_dir):
-    os.mkdir(data_dir)
-
-if not os.listdir(data_dir):
-    print("Data folder is empty. Provide some photos, then run trianer again")
-    quit()
-
-
-train_images = []
-
-for file in os.scandir(data_dir):
-    image = cv2.imread(file.path)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    #print(image)
-    train_images.append(image)
-
-
-train_images = np.array(train_images)
-
-IMAGE_SIZE = 120
-EPOCHS = 1000
-noise_dim = 200
-
-#BUFFER_SIZE = 60000
-#BATCH_SIZE = 256
-
-BUFFER_SIZE = 16
-BATCH_SIZE = 2
-
-#(train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
-
-print(train_images.shape)
-train_images = train_images.reshape( -1, IMAGE_SIZE, IMAGE_SIZE, 1).astype('float32')
-train_images = (train_images - 127.5) / 127.5  # Normalize the images to [-1, 1]
-
-
-train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
-
-
 def discriminator_loss(real_output, fake_output):
     real_loss = cross_entropy(tf.ones_like(real_output), real_output)
     fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
@@ -84,7 +40,7 @@ def train_step(images):
     discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
 
 
-def train(dataset, epochs):
+def train(dataset, epochs, checkpoints):
     for epoch in range(epochs):
         start = time.time()
 
@@ -93,31 +49,70 @@ def train(dataset, epochs):
 
 
         # Save the model every 15 epochs
-        if (epoch + 1) % 200 == 0 or (epoch + 1) == epochs:
+        if (epoch + 1) % checkpoints == 0 or (epoch + 1) == epochs:
             checkpoint.save(file_prefix = checkpoint_prefix)
-
-
-        
-
         print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
 
 
 print("START")
 
-generator = make_generator_model(IMAGE_SIZE, noise_dim)
-discriminator = make_discriminator_model(IMAGE_SIZE)
+if __name__ == '__main__':
 
-cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+    checkpoint_dir = './training_checkpoints'
+    if not os.path.exists(checkpoint_dir):
+        os.mkdir(checkpoint_dir)
 
-generator_optimizer = tf.keras.optimizers.Adam(1e-4)
-discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+    data_dir = './data'
+    if not os.path.exists(data_dir):
+        os.mkdir(data_dir)
+
+    if not os.listdir(data_dir):
+        print("Data folder is empty. Provide some photos, then run trianer again.\nREMEMBER TO USE SQUARE, BLACK-WHITE IMAGES (FOR NOW)")
+        quit()
 
 
-checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
-checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
-                                discriminator_optimizer=discriminator_optimizer,
-                                generator=generator,
-                                discriminator=discriminator)
+    train_images = []
+    BUFFER_SIZE = 0
+    for file in os.scandir(data_dir):
+        image = cv2.imread(file.path)
+        IMAGE_SIZE = image.shape[0]
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        train_images.append(image)
+        BUFFER_SIZE += 1
 
 
-train(train_dataset, EPOCHS)
+    train_images = np.array(train_images)
+
+    EPOCHS = int(input("How many epochs you want to train network?: "))
+    BATCH_SIZE = int(input("How big should batch be? You have {} photos: ".format(BUFFER_SIZE)))
+    noise_dim = int(input("What noise size you want to use?: "))
+    CHECKPOINT_EPOCH = int(input("How often do you want to make a checkpoint(in EPOCHS)?: "))
+
+    
+
+    #(train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
+
+    print(train_images.shape)
+    train_images = train_images.reshape( -1, IMAGE_SIZE, IMAGE_SIZE, 1).astype('float32')
+    train_images = (train_images - 127.5) / 127.5 
+
+
+    train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+
+    generator = make_generator_model(IMAGE_SIZE, noise_dim)
+    discriminator = make_discriminator_model(IMAGE_SIZE)
+
+    cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+    generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+    discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+
+
+    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+    checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                    discriminator_optimizer=discriminator_optimizer,
+                                    generator=generator,
+                                    discriminator=discriminator)
+
+
+    train(train_dataset, EPOCHS,CHECKPOINT_EPOCH )
